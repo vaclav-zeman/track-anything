ad8deb75fcd6ad94033807318924694dc494f6fe
Vaclav Zeman <vaclav.zeman@avast.com>
2019-03-16 13:54:27 +0100

    Update Header UI

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+
+const Wrap = styled.header`
+  text-align: center;
+  padding: 15px 0;
+`;
+
+const StyledLink = styled(Link)`
+  margin: 0 20px;
+  color: black;
+  text-decoration: none;
+  padding: 6px 10px;
+  display: inline-block;
+
+  ${(props: { primary?: boolean }) =>
+    props.primary &&
+    css`
+      background: #00b3e6;
+      color: white;
+      border-radius: 5px;
+    `}
+`;
 
 const Header = () => (
-  <header>
-    <Link to="/">Trackers</Link>
-    <Link to="/add">Add</Link>
-    <Link to="/stats">Stats</Link>
-  </header>
+  <Wrap>
+    <StyledLink to="/">Trackers</StyledLink>
+    <StyledLink primary to="/add">
+      Add +
+    </StyledLink>
+    <StyledLink to="/stats">Stats</StyledLink>
+  </Wrap>
 );
 
 export default Header;
